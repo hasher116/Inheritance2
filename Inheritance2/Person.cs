0c96ec65ca097df1c10f867af9da348281a470ff
hasher116 <andrei13081996@mail.ru>
2021-08-09 04:03:41 +0300

    Добавьте файлы проекта.

---
+++ Inheritance2/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public Person(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Person(string name)");
+        }
+        public Person(string name, int age) : this (name)
+        {
+            this.age = age;
+            Console.WriteLine("Person(string name, int age) : this (name)");
+        }
+    }
+}
---
+++ Inheritance2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inheritance2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person andrew = new Person("andrew");
+            Person igor = new Person("igor", 28);
+            Person vasya = new Employee("vasya", 26, "journalist");
+        }
+    }
+}
